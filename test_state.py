f4371e748514918dbca082689f3e4b7749f09d26
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-03-22 13:55:42 -0700

    added to gitignore

--- test_state.py
+++
@@ -1,8 +0,0 @@
-from models.base_model import BaseModel
-from models import storage
-from models.state import State
-
-all_objs = storage.all(State)
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
