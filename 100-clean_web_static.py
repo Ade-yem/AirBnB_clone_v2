4f42deefb9f689a096aa3891357a8d67a23fb402
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-07 11:53:51 -0700

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" Function that deploys """
+from fabric.api import *
+
+
+env.hosts = ['100.26.235.207', '54.86.208.71']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ CLEANS """
+
+    number = int(number)
+
+    if number < 1:
+        number = 1
+    elif number == 1:
+        number = 2
+    else:
+        number += 1
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
