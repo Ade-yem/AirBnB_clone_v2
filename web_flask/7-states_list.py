d60ec8ee50e7a6510c8d05f95ff12791ef8bb6a4
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-22 17:04:21 +0100

    changes to the older problems in the repo

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/env/python3
+"""starts a Flask web application listening on  0.0.0.0, port 5000"""
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def show_states():
+    """shows the list of states"""
+    objs = storage.all(States)
+    return render_template("7-states_list.html", objs=objs)
+
+@app.teardown_appcontext()
+def close(exception):
+    """remove current db session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
