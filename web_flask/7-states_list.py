4f639b5b7d011ec7da4e2b6c5b7ec352a9b4b111
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-24 12:40:01 -0700

    fixed interpreter

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,4 @@
-#!/usr/env/python3
+#!/usr/bin/python3
 """starts a Flask web application listening on  0.0.0.0, port 5000"""
 from flask import Flask, render_template
 from markupsafe import escape
