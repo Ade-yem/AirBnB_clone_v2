67c856c705c90a105d57e665e7cb72d9f0837d69
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-24 12:48:46 -0700

    fixed the failures

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""starts a Flask web application listening on  0.0.0.0, port 5000"""
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from flask import Flask, render_template
-from markupsafe import escape
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close(exception):
-    """remove current db session"""
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def show_states():
-    """shows the list of states"""
-    objs = storage.all(State)
-    return render_template("7-states_list.html", objs=objs)
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
